091cb2f1db4029ab02a32ab842cc665ec2be8638
lbarco248-glitch <lbarco248@gmail.com>
2026-04-15 16:07:40 -0500

    Add average, lowest, and highest test score values.

---
+++ src/main/java/com/projectmoonfan/app.java
@@ -0,0 +1,25 @@
+package com.projectmoonfan;
+
+import java.util.Arrays;
+
+public class app
+{
+    public static void main(String[] args)
+    {
+        int[] scores = {65, 78, 92, 55, 40, 86, 74, 88, 86, 96};
+        Arrays.sort(scores);
+        int total = 0;
+        for (int score = 0; score < scores.length; score = score + 1)
+        {
+            total += scores[score];
+        }
+        double average = (double) total / scores.length;
+        int highest = scores.length - 1;
+        System.out.printf("The average score of the class is %.1f%%.", average);
+        System.out.println();
+        System.out.printf("The lowest score of the class is %.1f%%.", (float) scores[0]); //Index 0 is lowest value
+        System.out.println();
+        System.out.printf("The highest score of the class is %.1f%%.", (float) scores[highest]);
+    }
+
+}
